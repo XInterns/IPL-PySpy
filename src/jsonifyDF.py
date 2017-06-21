a69ef5416700f6548b97cd09e75c925977ad4a82
Necrote <patel.vivek14@stu.upes.ac.in>
2017-06-21 14:15:02 +0530

    added restful api for performance consistency module

--- src/jsonifyDF.py
+++ src/jsonifyDF.py
@@ -12,8 +12,11 @@ sc = SparkContext("local","jsonifyApp")
 sql = SQLContext(sc)
 
 data_path = "../input/csv/"                                # path directory to input csv files
-data_opath = "../output/json/"                               # path directory to output csv files
+data_opath = "../output/csv/"                               # path directory to output csv files
 
+
+
+########### Common Modules ###########
 def getMatchDF():
 	match_rdd = sc.textFile(data_path + "matches.csv")         # reading csv files into RDD
 
@@ -37,11 +40,72 @@ def toJsonObj(srcDF):
         obj = srcDF_list[i]
         json_obj.append(dict(ast.literal_eval(obj)))
     return json_obj
-    
-def overall_rank_jsonify(season_num, srcDF):
-    srcDF = srcDF.filter(srcDF.season == season_num)\
+
+
+
+########### Overall Standings Module ###########
+def get_overall_ranks_df(season_num, srcDF):
+    overall_ranking = srcDF.filter(srcDF.season == season_num)\
                     .groupBy("winner").count().orderBy("count",ascending=0) # extracting required columns into another DF
-    srcDF = srcDF.filter("winner != '' ")                          # Deleting records of tied matches
-    srcDF = srcDF.selectExpr("winner as Team", "count as Wins")   # Renaming columns
+        
+    overall_ranking = overall_ranking.filter("winner != '' ")                          # Deleting records of tied matches
+    overall_ranking = overall_ranking.selectExpr("winner as Team", "count as Wins")   # Renaming columns
+    return overall_ranking
+
+def overall_rank_jsonify(srcDF, season_num):
+    result_DF = get_overall_ranks_df(season_num, srcDF)
+    return toJsonObj(result_DF)
+
+
+
+########### Performance Consistency Module ###########
+def get_consistency_DF(srcDF, season_lbound, season_ubound):    
+    consistency_df = srcDF.select("season","winner")\
+    .groupBy("season","winner").count().orderBy("winner")                  # extracting required columns 
+
+    consistency_df = consistency_df.filter("winner!='' ")                  # filtering out tied matches records
     
-    return toJsonObj(srcDF)
+    cond1 = func.col("season") >= season_lbound 
+    cond2 = func.col("season") <= season_ubound
+    consistency_df = consistency_df.filter(cond1 & cond2) 
+    return consistency_df
+
+
+def get_constraints(srcDF):
+    # constraint : teams that haven't played more than three season aren't considered
+    constraint_df = srcDF.groupBy("winner","season")\
+                .count().orderBy("winner")                                 # extracting list of season-wise winner teams
+ 
+    constraint_df = constraint_df.groupBy("winner").count()\
+                .filter("count>3 and winner!='' ")                       # filtering out teams that don't satisfy constraint
+    return constraint_df
+
+
+def filter_using_constraints(consistency_df, constraint_list):
+    consistency_df = consistency_df.where(func.col("winner")\
+                    .isin(constraint_list))                                # applying the constraint list
+    
+    consistency_df = consistency_df.groupBy("winner")\
+                    .agg(func.stddev_pop("count").alias("stddev"),\
+                    func.sum("count").alias("total_wins"))\
+                    .orderBy("stddev","total_wins")                        # calculating the performance consistency
+    return consistency_df
+
+
+def calc_consistency(consistency_df):
+    consistency_df = consistency_df.withColumn("final_deviations",\
+                    ((10-consistency_df.stddev)/10)*100)\
+                    .orderBy("final_deviations", ascending=False)          # scaling to appropriate scale
+        
+    consistency_df = consistency_df.selectExpr("winner as Teams", "final_deviations as Consistency")    
+    return consistency_df
+
+
+def consistency_jsonify(srcDF, season_lbound = 2008, season_ubound = 2016):
+    result_DF = get_consistency_DF(srcDF, season_lbound, season_ubound)      # extracting required columns 
+    constraints_df = get_constraints(srcDF)
+    constraints_list = [i.winner for i in constraints_df.collect()]          # storing a list of filtered teams
+    result_DF = filter_using_constraints(result_DF, constraints_list)
+    result_DF = calc_consistency(result_DF)
+
+    return toJsonObj(result_DF)
--- src/pyspy_restful_api.py
+++ src/pyspy_restful_api.py
@@ -1,5 +1,6 @@
 from jsonifyDF import *
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 mdf = getMatchDF()
 
@@ -11,7 +12,16 @@ def test():
 def returnOverallStandings():
 	args = request.args
 	season = args['season']
-	return jsonify({"Overall_Standings_"+season: overall_rank_jsonify(season,mdf)})
+	return jsonify({"Overall_Standings_"+season: overall_rank_jsonify(mdf, season)})
+
+@app.route("/consistency", methods=["GET"])
+def returnPerformanceConsistencies():
+	args = request.args
+	lbound = int(args['lbound'])
+	ubound = int(args['ubound'])
+	print lbound
+	print ubound
+	return jsonify({"Performance_Consistency_"+str(lbound)+"_to_"+str(ubound): consistency_jsonify(mdf, lbound, ubound)})
 
 if __name__ == "__main__":
 	app.run(port=5000)
