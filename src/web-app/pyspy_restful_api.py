3f56038a5a45a338323808516bf67771f24e7b2e
kris0107 <mr.rajansinghal@gmail.com>
2017-06-22 12:48:24 +0530

    added player performance in api with json

--- src/web-app/pyspy_restful_api.py
+++ src/web-app/pyspy_restful_api.py
@@ -10,7 +10,9 @@ def test():
 	<b>Modules directory:</b></br>
 	<a href="/overallstandings/help">Team Overall Standings</a></br>
 	<a href="/consistency/help">Team Performance Consistency</a></br>
-	<a href="/TeamVsTeamWinPercentage/help">Team V/S Team Win Percentage</a></br>'''
+	<a href="/TeamVsTeamWinPercentage/help">Team V/S Team Win Percentage</a></br>
+	<a href="/PlayerPerformance/help">Player Performance</a></br>'''
+
 
 @app.route("/overallstandings/help", methods=["GET"])
 def returnOverallStandingsHelp():
@@ -48,6 +50,13 @@ def returnTeamVsTeamWinPercentageHelp():
 		<li>Kings XI Punjab</li>
 	</ul>'''
 
+@app.route("/PlayerPerformance/help", methods=["GET"])
+def returnPlayerPerformanceHelp():
+	return ''' <h1>**** Player Performance ****</h1></br>
+	<b>about:</b> returns json data of overall performance for each player</br>
+	<b>usage:</b> /PlayerPerformance?player=V+Kohli</br>'''
+
+
 @app.route("/overallstandings", methods=["GET"])
 def returnOverallStandings():
 	args = request.args
@@ -67,6 +76,12 @@ def returnTeamVsTeamWinPercentage():
 	team1 = args['team1']
 	team2 = args['team2']
 	return jsonify({"Team_Vs_Team_Win_Percentage_"+team1+"_VS_"+team2: team_vs_team_jsonify(mdf, team1, team2)})
+
+@app.route("/PlayerPerformance", methods=["GET"])
+def returnPlayerPerformance():
+	args = request.args
+	player = args['player']
+	return jsonify({"Player_Performance_"+player: Player_Performance_jsonify(player)})
 
 if __name__ == "__main__":
 	app.run(port=5000)
